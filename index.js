629525c01002fc7566758522b44ef9a0e9dd3382
Raadiyah Arend <raadiyaharend6@gmail.com>
2023-08-21 14:13:45 +0200

    error messages and and installed flash-connect

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import exphbs from 'express-handlebars';
+import session from 'express-session';
+import flash  from 'connect-flash';
+import pgPromise from 'pg-promise';
+
 import MainGreetings from './functions/greetings.js';
 
+const pgp = pgPromise();
 const app = express();
 const greetings = MainGreetings();
 const port = process.env.PORT || 3012;
@@ -19,33 +24,38 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//routes
+app.use(session({
+    secret: 'your-secret-key',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(flash());
 
+
+//routes
 
 app.get('/', function (req, res) {
 
-    // const greeting = greetings.greetingTheUser();
-
     const greetingTheUser = greetings.greetingTheUser();
     const counter = greetings.amountOfUsers();
-    // const getCounter = greetings.getCounter();
-    // const names = greetings.getNames();
-    // const errorMessages = greetings.errorMessages();
-    console.log(greetingTheUser)
+   
+
+    const errorMessage = req.flash();
+    console.log(req.flash('error'));
+    // console.log(greetingTheUser)
     res.render('index', {
         counter,
         greetingTheUser,
-        // getCounter,
-        // names,
-        // errorMessages
-
+        errorMessage
     });
 });
 
 app.post('/greetings', function (req, res) {
-   
+
     const name = req.body.greetedName;
     const language = req.body.language;
+
+    req.flash('error', greetings.errorMessages(name, language));
 
     greetings.setName(name);
     greetings.setLanguage(language);
@@ -53,15 +63,24 @@ app.post('/greetings', function (req, res) {
     res.redirect('/')
 })
 
-app.get('/greeted',function (req, res){
-    
+app.get('/greeted', function (req, res) {
+
     const greetedNames = greetings.getNames();
 
-    res.render("greeted",{ greeted: greetedNames })
+    res.render("greeted", { greeted: greetedNames })
 })
 
 
+app.post('/reset', function (req, res) {
 
+    const resetingCounter = greetings.resetCounter();
+
+    greetings.resetCounter(resetingCounter);
+
+    res.redirect('/')
+})
+
+//Port
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
